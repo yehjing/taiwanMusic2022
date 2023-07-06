274fcc3df218a3b4f35718c08a28dc59210291d4
Austen <twausten@ko-noha.com>
2023-07-06 11:33:09 +0800

    影片

--- src/components/videoRecord/video_child/video_1.vue
+++ src/components/videoRecord/video_child/video_1.vue
@@ -2,7 +2,7 @@
   <div class="videoContent">
     
     <h1>振聲社「振以傳聲：振聲社傳藝成果展暨臺南市南管音樂保存者蔡芬得授證典禮」</h1>
-<iframe frameborder="0" allowfullscreen="" scrolling="no" src="https://onelineplayer.com/player.html?autoplay=false&autopause=false&muted=false&loop=true&url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fauthuser%3D0%26id%3D1HqL3d_tLkvXiAiDnbmFTt8LIZFkTSZ86%26export%3Ddownload&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true" width="560" height="315"></iframe>
+<iframe frameborder="0" allowfullscreen="" scrolling="no" src="https://onelineplayer.com/player.html?autoplay=false&autopause=false&muted=false&loop=true&url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fauthuser%3D0%26id%3D1h-0YtklujERJUaP6Cmv1d91QPyuiiqR_%26export%3Ddownload&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true" width="560" height="315"></iframe>
 
     <h1>臺南市南聲社「風神月色．雅樂相逢：南聲社展演推廣音樂會暨臺南市傳統表演藝術南管音樂保存者張栢仲授證典禮」</h1>
     <iframe frameborder="0" allowfullscreen="" scrolling="no" width="560" height="315" src="https://onelineplayer.com/player.html?autoplay=false&autopause=false&muted=false&loop=true&url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fauthuser%3D0%26id%3D1h-0YtklujERJUaP6Cmv1d91QPyuiiqR_%26export%3Ddownload&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true"></iframe>
