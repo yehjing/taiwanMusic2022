3beb6d966189e0b818741a42263bb86490ff6d88
jingyeh <>
2023-08-15 23:20:35 +0800

    換雲端

--- src/components/videoRecord/video_child/video_13.vue
+++ src/components/videoRecord/video_child/video_13.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="videoContent">
     <h1>依拜維吉Ipay Buyci〈1234 qutux, saying, tyugan, payat〉官方版MV</h1>
-    <iframe frameborder="0" allowfullscreen="" scrolling="no" width="560" height="315" src="https://onelineplayer.com/player.html?autoplay=false&autopause=false&muted=false&loop=true&url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fauthuser%3D0%26id%3D1xcJ_M_IWadi4UEkMagbs3MRL2pZMeMyc%26export%3Ddownload&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true"></iframe>
+    <iframe frameborder="0" allowfullscreen="" scrolling="no" width="560" height="315" src="https://onelineplayer.com/player.html?autoplay=false&autopause=false&muted=false&loop=true&url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fauthuser%3D0%26id%3D1HiH51BE_ssqDOYhZlBRU7YuGypUSAj3i%26export%3Ddownload&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true"></iframe>
     <h1>2022搖滾臺中、雷霆電司音樂祭</h1>
-    <iframe frameborder="0" allowfullscreen="" scrolling="no" width="560" height="315" src="https://onelineplayer.com/player.html?autoplay=false&autopause=false&muted=false&loop=true&url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fauthuser%3D0%26id%3D1UPx_XTk-gUvgYo0i6kKucqroLiwQaPQq%26export%3Ddownload&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true"></iframe>
+    <iframe frameborder="0" allowfullscreen="" scrolling="no" width="560" height="315" src="https://onelineplayer.com/player.html?autoplay=false&autopause=false&muted=false&loop=true&url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fauthuser%3D0%26id%3D13aAFfknk5wKH_T888DoAxToKA78Bo_Iu%26export%3Ddownload&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true"></iframe>
     <h1>臺南市政府「再見阿文哥－文夏紀念音樂會」</h1>
-    <iframe frameborder="0" allowfullscreen="" scrolling="no" width="560" height="315" src="https://onelineplayer.com/player.html?autoplay=false&autopause=false&muted=false&loop=true&url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fauthuser%3D0%26id%3D1hAhv9yEagXN_S3aj1rYGQnxh9EkQLPXY%26export%3Ddownload&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true"></iframe>
+    <iframe frameborder="0" allowfullscreen="" scrolling="no" width="560" height="315" src="https://onelineplayer.com/player.html?autoplay=false&autopause=false&muted=false&loop=true&url=https%3A%2F%2Fdrive.google.com%2Fuc%3Fauthuser%3D0%26id%3D1xJM5kA-uPdj7iXJD-amTAASwG58cYlPE%26export%3Ddownload&poster=&time=true&progressBar=true&overlay=true&muteButton=true&fullscreenButton=true&style=light&quality=auto&playButton=true"></iframe>
   </div>
 </template>
 <style scoped>
